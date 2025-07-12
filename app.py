4172a9ed899168ee61636cb4d24cc0706566aaec
Narepalemteja <tejanarepalem@gmail.com>
2025-07-12 11:23:31 +0530

    Fix for Render deployment: bind to 0.0.0.0 and dynamic port

--- app.py
+++ app.py
@@ -75,3 +75,9 @@ def edit_student(id):
 if __name__ == '__main__':
     init_db()
     app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    init_db()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
